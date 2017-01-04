2dbc42b7e67f72be89ec806c9212cb58d072d35e
blubberly <manuel-manuel@web.de>
2017-01-04 16:01:30 +0100

    foo: bar

--- server.js
+++ server.js
@@ -35,3 +35,22 @@ io.sockets.on('connection', function (socket) {
 
 // Portnummer in die Konsole schreiben
 console.log('Der Server läuft nun unter http://127.0.0.1:' + port + '/');
+
+
+//GOOGLE 
+/*
+var GoogleAuth = require('google-auth-library');
+var auth = new GoogleAuth;
+var client = new auth.OAuth2(CLIENT_ID, '', '');
+client.verifyIdToken(
+    token,
+    CLIENT_ID,
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3],
+    function(e, login) {
+      var payload = login.getPayload();
+      var userid = payload['sub'];
+      // If request specified a G Suite domain:
+      //var domain = payload['hd'];
+    });
+    */
